7758bb0afd94f128daea51a334f8754d9b0880ea
ELAKIYAN <31242650+ELAKIYAN@users.noreply.github.com>
2018-02-02 16:24:16 +0530

    ALPHABHATE.C

---
+++ ALPHABHATE.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+char a;
+printf("enter the letter");
+scanf("%c",&a);
+if((a>='a')&&(a<='z'))
+printf("letter is alphebet");
+else
+printf("letter is not alphabet");
+return 0;
+}
