c6d0caa55dffda7dbc91272dadcbe1cf1d9b24d4
ELAKIYAN <31242650+ELAKIYAN@users.noreply.github.com>
2018-02-02 16:43:08 +0530

    MAX.C

---
+++ MAX.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+int main()
+{
+int arr[MAX_SIZE];
+int i, n, sum=0;
+printf("Enter size of the array: ");
+scanf("%d", &n);
+printf("Enter %d elements in the array: ", n);
+for(i=0; i<n; i++)
+{
+scanf("%d", &arr[i]);
+sum += arr[i];
+}
+printf("Sum of all elements of array = %d", sum);
+return 0;
+}
