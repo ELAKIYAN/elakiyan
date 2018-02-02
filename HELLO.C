70b43619796146c9ca0d4e4499da5088d0ff3499
ELAKIYAN <31242650+ELAKIYAN@users.noreply.github.com>
2018-02-02 16:36:23 +0530

    HELLO.C

---
+++ HELLO.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char a[10];
+int i,c;
+printf("Enter the word:");
+scanf("%s",a);
+printf("\nenter the N times");
+scanf("%d",&c);
+for(i=0;i<=c;i++)
+{
+printf("\n%s",a);
+}
+
+}
