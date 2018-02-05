0a8fa5974ee2c9d4fdabf49e9705e9a437906423
ELAKIYAN <31242650+ELAKIYAN@users.noreply.github.com>
2018-02-05 16:26:13 +0530

    POSIC.C

---
+++ POSIC.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+    int n, i, a = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    for(i=2; i<=n/2; ++i)
+    {
+        if(n%i==0)
+        {
+           a=1;
+            break;
+        }
+    }
+
+    if (a==0)
+        printf("%d is a prime number.",n);
+    else
+        printf("%d is not a prime number.",n);
+        
+        }
