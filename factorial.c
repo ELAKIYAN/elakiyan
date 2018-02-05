c6c8a74aff79be43a8ffd9f7ba07f865e711b373
ELAKIYAN <31242650+ELAKIYAN@users.noreply.github.com>
2018-02-05 16:38:41 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int fact=i,n;
+ printf("\n enter the number");
+ scanf("%d",&n);
+ for(int i=1;i<=n;i++)
+ {
+ fact=fact*i;
+ }
+ printf(" the factorial of the given number is:%d",&fact);
+ getch();
+ }
