b299c49d5a10f2cd6e8da2c936107bee15aeaaff
ELAKIYAN <31242650+ELAKIYAN@users.noreply.github.com>
2018-02-05 16:37:03 +0530

    Update arm 2 interval.c

--- arm 2 interval.c
+++ arm 2 interval.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 void main()
 {
-int low, high, i, temp1, temp2, remainder, n = 0;
-printf("Enter two numbers(intervals): ");
-    scanf("%d %d", &low, &high);
-    printf("Armstrong numbers between %d an %d are: ", low, high);
-
-    for(i = low + 1; i < high; ++i)
+ int low, high, i, temp1, temp2, remainder, n = 0;
+ printf("Enter two numbers(intervals): ");
+   scanf("%d %d", &low, &high);
+   printf("Armstrong numbers between %d an %d are: ", low, high);
+   for(i = low + 1; i < high; ++i)
     {
-        temp2 = i;
+       temp2 = i;
         temp1 = i;
         while (temp1 != 0)
         {
