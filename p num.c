ec4169755b149535483dc28aecc62603210c02eb
ELAKIYAN <31242650+ELAKIYAN@users.noreply.github.com>
2018-02-05 16:31:16 +0530

    Create p num.c

---
+++ p num.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+int low, high, i, flag, temp;
+ printf("Enter two numbers(intevals): ");
+ scanf("%d %d", &low, &high);
+if (low > high) {
+temp = low;
+low = high;
+high = temp;
+}
+ printf("Prime numbers between %d and %d are: ", low, high);
+while (low < high)
+{
+flag = 0;
+ for(i = 2; i <= low/2; ++i)
+{
+if(low % i == 0)
+{
+flag = 1;
+break;
+}
+}
+if (flag == 0)
+printf("%d ", low);
+++low;
+}
+ return 0;
+ }
